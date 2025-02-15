8010f54a24c65863b8750b712351e9abb48845c0
ilhamdika <ilham27dika@gmail.com>
2025-02-15 15:07:40 +0700

    fix: transaction list

--- src/Pages/Transaction.jsx
+++ src/Pages/Transaction.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import Info from "../components/Info";
 import CardTransaction from "../components/CardTransaction";
 
@@ -6,23 +6,24 @@ export const Transaction = () => {
   const apiUrl = import.meta.env.VITE_API_URL + "transaction/history";
   const [transactions, setTransactions] = useState([]);
   const [offset, setOffset] = useState(0);
-  const [limit] = useState(3);
+  const [limit] = useState(5);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
-  const isMounted = useRef(false);
 
   useEffect(() => {
-    if (!isMounted.current) {
-      isMounted.current = true;
-      return;
+    fetchTransactions(0);
+  }, []);
+
+  useEffect(() => {
+    if (offset > 0) {
+      fetchTransactions(offset);
     }
-    fetchTransactions(offset);
   }, [offset]);
 
   const fetchTransactions = async (currentOffset) => {
     if (loading || !hasMore) return;
-
     setLoading(true);
+
     try {
       const token = localStorage.getItem("token");
       if (!token) {
@@ -45,7 +46,6 @@ export const Transaction = () => {
 
         setTransactions((prev) => {
           const uniqueTransactions = newTransactions.filter((newTrx) => !prev.some((prevTrx) => prevTrx.invoice_number === newTrx.invoice_number));
-
           return [...prev, ...uniqueTransactions];
         });
 
@@ -72,7 +72,7 @@ export const Transaction = () => {
         <h3 className="text-2xl font-bold mb-4 text-gray-800 dark:text-gray-200">Semua Transaksi</h3>
 
         {transactions.length > 0 ? (
-          transactions.map((trx, index) => (
+          transactions.map((trx) => (
             <CardTransaction key={trx.invoice_number} nominal={trx.total_amount} tanggal={new Date(trx.created_on).toLocaleDateString("id-ID")} keterangan={trx.description} tipe={trx.transaction_type.toLowerCase()} />
           ))
         ) : (
